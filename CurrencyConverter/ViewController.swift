e5151de396cd929e3c47769b8e7e1c525d64d2de
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-01 08:54:37 +0300

    1. Request & Session

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -10,15 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var cadLabel: UILabel!
-    
     @IBOutlet weak var chfLabel: UILabel!
-    
     @IBOutlet weak var gbpLabel: UILabel!
-    
     @IBOutlet weak var jpyLabel: UILabel!
-    
     @IBOutlet weak var usdLabel: UILabel!
-    
     @IBOutlet weak var tryLabel: UILabel!
     
     
@@ -29,6 +24,24 @@ class ViewController: UIViewController {
 
 
     @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // 1. Request & Session
+        // 2. Response & Data
+        // 3. Parsing & JSON Serialization
+        
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
+        let session = URLSession.shared
+        //Closure
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error!", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK!", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+            } else {
+                
+            }
+        }
     }
 }
 
